3ab5cc9c36a2c70077b8d1b3fc04113dbd9b3c36
ibrahim-bkrn <ibrahim.boukernafaa@gmail.com>
2025-09-23 10:44:56 +0200

    modif 4

--- src/Controller/acceuilController.php
+++ src/Controller/acceuilController.php
@@ -2,17 +2,43 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\TrotElec;
+use App\Repository\TrotElecRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class acceuilController extends AbstractController {
 
     #[Route('/acceuil')]
     public function acceuil() {
-        return $this->render('acceuil.html');
+        return $this->render('acceuil.html.twig');
+    }
 
+    #[Route('/ajoutTrot', name : 'app_ajout_trot')]
+    function ajtTrot(EntityManagerInterface $em) {
+        $nvTrot = new TrotElec();
+        $nvTrot->setModel("Xiaomi Mi Electric Scooter Pro 2");
+
+        $em->persist($nvTrot);
+        $em->flush();
+
+        return new Response("Trotinnette ajouté avec succès !");
+    }
+
+    #[Route('/suppTrot', name : 'app_supp_trot')]
+    function suppTrot(EntityManagerInterface $em, TrotElecRepository $trot) {
+        $trotsASupprimer = $trot->findAll(); 
+
+        foreach ($trot as $trotsASupprimer) {
+            $em->remove($trot);        
+        }
+
+        $em->flush();
+
+        return new Response("Trotinnette supprimée avec succès !");
     }
 }
